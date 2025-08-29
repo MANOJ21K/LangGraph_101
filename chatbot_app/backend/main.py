8ca1a2853d396b1b5ed99e7ccbfe3806812e8434
Manoj21k <manojkotary@gmail.com>
2025-08-29 15:46:29 +0530

    feat: replaced langgraph memory saver with sqlite db to store conversations

--- chatbot_app/backend/main.py
+++ chatbot_app/backend/main.py
@@ -2,7 +2,9 @@ from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Literal, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage, AIMessage
 # for memory management
-from langgraph.checkpoint.memory import MemorySaver
+#from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 from langgraph.graph.message import add_messages
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
@@ -65,5 +67,9 @@ graph.add_node('chat_node', chat_node)
 graph.add_edge(START, 'chat_node')
 graph.add_edge('chat_node', END)
 
-checkpointer = MemorySaver()
+
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+
+checkpointer = SqliteSaver(conn=conn)
+
 workflow = graph.compile(checkpointer=checkpointer)
